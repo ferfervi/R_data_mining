e223fb3737688dd484e7d61e2efc9212cb25addb
ferfervi <ferfervi@gmail.com>
2015-10-30 14:23:36 +0100

    performing a location analysis after capturing tweets using the real time API from tweeter

--- RTwitterStreamingAPI.R
+++ RTwitterStreamingAPI.R
@@ -44,6 +44,39 @@ setTwitterConnection = function(filename)
 }
 
 
+# C) Capture Twitts using the Sreaming API
+captureTweetsRealTime = function(case)
+{
+      #Select wich case to RUN:
+      switch(case, 
+          {# C.1) Streaming, parsing tweets case 1.
+            
+            #Empty previous tweets
+            #if(file.exists("tweets.json")) file.remove("tweets.json")
+            
+            filterStream(file.name = "tweets.json", # Save tweets in a json file
+                         track = c("ForcadellTV3","#ForcadellTv3"), # Collect tweets mentioning either Affordable Care Act, ACA, or Obamacare
+                         timeout = 3600, # Keep connection alive for 60 seconds
+                         oauth = my_oauth) # Use my_oauth file as the OAuth credentials
+            tweets.df <- parseTweets("tweets.json", simplify = FALSE) # parse the json file and save to a data frame called tweets.df. Simplify = FALSE ensures that we include lat/lon information in that data frame.
+          }, 
+          
+          {# C.2) Streaming, parsing tweets case 2. 
+            
+            #Empty previous tweets
+            if(file.exists("tweets.json")) file.remove("tweets.json")
+            
+            filterStream(file.name = "tweets.json", # Save tweets in a json file
+                         track = c("FCBarcelona", "futbol"), # Collect tweets mentioning either Affordable Care Act, ACA, or Obamacare
+                         language = "es",
+                         timeout = 30, # Keep connection alive for 60 seconds
+                         oauth = my_oauth) # Use my_oauth file as the OAuth credentials
+            tweets.df <- parseTweets("tweets.json", simplify = FALSE) # parse the json file and save to a data frame called tweets.df. Simplify = FALSE ensures that we include lat/lon information in that data frame.
+          }
+          
+        )
+}
+
 
 ######## INIT - SET CONNECTION #####
 
@@ -59,34 +92,26 @@ load("my_oauth.Rdata")
 
 ######################
 
-# C) Capture Twitts using the Sreaming API
-#Select wich case to RUN:
-switch(1, 
-       {# C.1) Streaming, parsing tweets case 1.
-         
-         #Empty previous tweets
-         #if(file.exists("tweets.json")) file.remove("tweets.json")
-         
-         filterStream(file.name = "tweets.json", # Save tweets in a json file
-                      track = c("ForcadellTV3","#ForcadellTv3"), # Collect tweets mentioning either Affordable Care Act, ACA, or Obamacare
-                      timeout = 3600, # Keep connection alive for 60 seconds
-                      oauth = my_oauth) # Use my_oauth file as the OAuth credentials
-         tweets.df <- parseTweets("tweets.json", simplify = FALSE) # parse the json file and save to a data frame called tweets.df. Simplify = FALSE ensures that we include lat/lon information in that data frame.
-       }, 
-       
-      {# C.2) Streaming, parsing tweets case 2. 
-        
-        #Empty previous tweets
-        if(file.exists("tweets.json")) file.remove("tweets.json")
-        
-        filterStream(file.name = "tweets.json", # Save tweets in a json file
-                     track = c("FCBarcelona", "futbol"), # Collect tweets mentioning either Affordable Care Act, ACA, or Obamacare
-                     language = "es",
-                     timeout = 30, # Keep connection alive for 60 seconds
-                     oauth = my_oauth) # Use my_oauth file as the OAuth credentials
-        tweets.df <- parseTweets("tweets.json", simplify = FALSE) # parse the json file and save to a data frame called tweets.df. Simplify = FALSE ensures that we include lat/lon information in that data frame.
-      }
-              
-       )
+# C) Capture Twitts using the Sreaming API. Uncomment to start capturing (blocking-real time)
+# captureTweetsRealTime(1)
+
+# D) Process tweets. Analysis.
+
+# D.1) Plot location from where were made the tweets. Note that this is not exact since we are trying
+# to read the field where the user wrote that he/she is living and later trying to figure out the coordenates.
+library(ggmap)
+library(ggplot2)
+
+tweetsToAnalyze <- parseTweets("tweets.json", simplify = FALSE) # parse the json file and save to a data frame called tweets.df. Simplify = FALSE ensures that we include lat/lon information in that data frame.
+locatedUsers <- !is.na(tweetsToAnalyze$location)
+locations <- geocode(tweetsToAnalyze$location[locatedUsers])  # Use amazing API to guess
 
 
+# Create a data frame with the locations (coordenates) to display in the map
+#d <- data.frame(lat=c(50.659631, 50.607213, 50.608129),lon=c(3.09319, 3.011473, 3.031529))
+df_locations <- data.frame(lat=locations$lat,lon=locations$lon)
+
+#Select map zone to display, size and paint points in the coordenates
+myMap <- get_map("Catalonia", zoom=8)
+p <- ggmap(myMap)
+p + geom_point(data=d, aes(x=lon, y=lat), color="red", size=6, alpha=0.3)
